310335dda735e2f54cafaf20497c3257586ac1f4
Dmytro20777 <08dmutro11@gmail.com>
2024-03-31 14:05:00 +0300

    h202-express

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,147 @@
+import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (_req, res) => {
+    try {
+        let users = await contactsService.listContacts();
 
-export const getOneContact = (req, res) => {};
+        if (typeof users === 'string') {
+            users = JSON.parse(users);
+        }
 
-export const deleteContact = (req, res) => {};
+        res.status(200).json({
+            message: "Success",
+            users: users
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
 
-export const createContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await contactsService.getContactById(userId);
 
-export const updateContact = (req, res) => {};
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Success",
+            user: user
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
+export const deleteContact = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await contactsService.removeContact(userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User non found"
+            });
+        }
+
+        res.status(200).json({
+                message: "Success"
+            })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
+
+export const createContact = async (req, res) => {
+    try {
+
+        const { error } = createContactSchema.validate(req.body);
+        
+        if (error) {
+            return res.status(400).json({
+                message: error.message
+            })
+        }
+
+        const { name, email, phone } = req.body;
+
+        const newUser = await contactsService.addContact(name, email, phone);
+
+        res.status(201).json({
+            message: "Contact successfully created",
+            newUser
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+};
+
+export const updateContact = async (req, res) => {
+    try {
+        const { error } = updateContactSchema.validate(req.body);
+        
+        if (error) {
+            return res.status(400).json({
+                message: error.message
+            });
+        }
+
+        const { id } = req.params;
+        const { name, email, phone } = req.body;
+
+        const existingContact = await contactsService.getContactById(id);
+
+        if (!existingContact) {
+            return res.status(404).json({
+                message: "Contact not found"
+            });
+        }
+
+        if (
+            name === existingContact.name &&
+            email === existingContact.email &&
+            phone === existingContact.phone
+        ) {
+            return res.status(400).json({
+                message: "Body must have at least one field"
+            })
+        }
+
+        const updateUser = await contactsService.updateContact(id, { name, email, phone });
+        
+        if (!updateUser) {
+            return res.status(404).json({
+                message: "Not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Contact successfully updated",
+            updateUser: updateUser
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
+
+
---
+++ services/contactsServices.js
@@ -0,0 +1,99 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { v4 as uuidv4 } from 'uuid';
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+async function listContacts() {
+    try {
+        const data = await fs.readFile(contactsPath);
+        return data.toString();
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find(contact => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+async function removeContact(contactId) {
+    try {
+        const data = await fs.readFile(contactsPath);
+        let contacts = JSON.parse(data);
+
+        const index = contacts.findIndex(contact => contact.id === contactId);
+        if (index !== -1) {
+            const removedContact = contacts.splice(index, 1)[0];
+            await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+            return removedContact;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+async function addContact(name, email, phone) {
+  try {
+      const data = await fs.readFile(contactsPath);
+      const contacts = JSON.parse(data);
+
+      const newContact = {
+          id: uuidv4(),
+          name: name,
+          email: email,
+          phone: phone
+      };
+
+      contacts.push(newContact);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+      return newContact;
+
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function updateContact(contactId, updatedData) {
+    try {
+        const data = await fs.readFile(contactsPath);
+        const contacts = JSON.parse(data);
+
+        const index = contacts.findIndex(contact => contact.id === contactId);
+
+        if (index === -1) {
+            throw new Error("Contact not found")
+        }
+
+        contacts[index] = {
+            ...contacts[index],
+            ...updatedData
+        }
+
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+        return contacts[index];
+
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export default { listContacts, getContactById, removeContact, addContact, updateContact };
+
+
+
